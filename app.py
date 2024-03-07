424d388fcb22e242be28c50fde4e36e54190e3f2
Luciano Borba <luhborbafilho@gmail.com>
2024-03-06 21:07:04 -0300

    feat: criando validação no arquivo app.py

--- app.py
+++ app.py
@@ -1,4 +1,33 @@
 import streamlit as st
+import pandas as pd
+from schema import ContratoFuncionarios
+
+def validar(file):
+    """
+    Função responsável pela validação do arquivo em formato csv de acordo com o contrado de dados.
+    
+    Args:
+        file: Arquivo csv.
+    """
+    try:
+        df = pd.read_csv(file)
+        erros = []
+        
+        for idx, row in df.iterrows():
+            try:
+                ContratoFuncionarios(**row.to_dict())
+            except Exception as e:
+                erros.append(f'Linha {idx+2}: {e}')
+        
+        if erros:
+            st.error('Erros encontrados no seu arquivo CSV:')
+            for erro in erros:
+                st.error(erro)
+        else:
+            st.success('Arquivo Validado com Sucesso!!!')
+    except Exception as e:
+        st.error(f'Erro ao ler o arquivo CSV: {e}')
+            
 
 def main():
     """
@@ -9,7 +38,12 @@ def main():
     
     st.title('Validação de CSV - DataQuality')
     
-    st.file_uploader('Selecione o arquivo CSV', type=['csv'])
+    csv = st.file_uploader('Selecione o arquivo CSV', type=['csv'])
+    
+    botao = st.button('Validar')
+    
+    if botao:
+        validar(csv)
     
 if __name__ == '__main__':
     main()
