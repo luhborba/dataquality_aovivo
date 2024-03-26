1c0294ad00a92a0dc7adf192644966904ca62bcd
LucianoBorba <luhborbafilho@gmail.com>
2024-03-26 20:55:33 -0300

    refactor: refatorando conforme as pep (isort, black, pydocstyle)

--- schema.py
+++ schema.py
@@ -1,7 +1,23 @@
-from pydantic import BaseModel, PositiveInt, EmailStr
+"""Arquivo de Contrato de Dados."""
+
 from datetime import datetime
 
+from pydantic import BaseModel, EmailStr, PositiveInt
+
+
 class ContratoFuncionarios(BaseModel):
+    """Classe de Contrato de Dados.
+
+    Arg:
+        id (PositiveInt): Identificador do registro
+        nome (str): Nome do funcionário
+        idade (PositiveInt): Idade do funcionário
+        datanascimento (datetime): Data de nascimento do funcionário
+        email (EmailStr): Email do funcionário
+        cargo (str): Cargo do funcionário
+        departamento (str): Departamento do funcionário
+    """
+
     id: PositiveInt
     nome: str
     idade: PositiveInt
